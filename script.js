b7bbd27551814058ef5f146fc7440e32246c4ee2
Pro Amedeo <proamedeo@MacBook-Pro-di-Pro.local>
2018-11-14 15:49:00 +0100

    non so come...ma funziona :-D

---
+++ script.js
@@ -0,0 +1,22 @@
+// creo la lista delle email
+var email = ['a', 'b', 'c'];
+// chiedo all'utente di inserire la sua email
+var emailUtente = prompt('Inserisci il tuo indirizzo email');
+// di base il mio login sarà errato
+var loginOK = false;
+// verifico che l'email dell'utente sia presente nella lista
+for (i = 0; i < email.length; i++){
+// ciclate le e-mail se almeno una delle condizioni si verifica allora trasformo il login in true
+  if (emailUtente == email[i]){
+
+    loginOK = true;
+
+  }
+}
+
+//effettuato ciclo e verifiche se loginok è true OK PUOI ENTRARE altrimenti NON SEI AUTORIZZATO
+ if (loginOK){
+   console.log("OK puoi entrare");
+ } else {
+   console.log("Non sei autorizzato all'accesso");
+ }
