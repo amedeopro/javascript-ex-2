654b3051e9c6fb201d47c4d87a946281c32b4e2a
Pro Amedeo <proamedeo@MacBook-Pro-di-Pro.local>
2018-11-14 16:53:22 +0100

    esercizio completato

---
+++ dadi.js
@@ -0,0 +1,15 @@
+// mi creo i dadi che generano numeri random da 1 a 6
+var dado1 = Math.floor(Math.random() * 6) + 1;
+var dado2 = Math.floor(Math.random() * 6) + 1;
+
+document.writeln("Giocatore 1 esce il numero  " + dado1 + "<br>" + "<br>");
+document.writeln("Giocatore 2 esce il numero  " + dado2 + "<br>" + "<br>");
+
+//creo le condizioni per tirar fuori un vincitore
+if (dado1 < dado2){
+  document.writeln("Vince il giocatore 2");
+} else if (dado1 > dado2){
+  document.writeln("Vince il giocatore 1");
+} else {
+  document.writeln("Pari! Nessuno vince");
+}
